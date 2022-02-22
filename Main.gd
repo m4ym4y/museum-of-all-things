28d58b66cbeff8fddf0582bd85b60154a33e9851
m4ym4y <m4ym4y@users.noreply.github.com>
2022-02-22 10:13:20 -0800

    create room layout purely from map

--- Main.gd
+++ Main.gd
@@ -17,78 +17,143 @@ const OPPOSING_WALL_MAP = {
 	"EastWall": "WestWall"
 }
 
+var ROOM_LIST = {}
+var ROOM_MAP = {
+	name = "Central Room",
+	width = 10,
+	length = 10,
+	height = 4,
+	doors = [
+		{
+			wall = "NorthWall",
+			left = 2,
+			width = 1,
+			height = 2,
+			room = {
+				name = "North Room",
+				width = 20,
+				length = 20,
+				height = 4,
+				doors = [
+					{
+						wall = "WestWall",
+						left = 5,
+						width = 1,
+						height = 2,
+						room = {
+							name = "North-West Room",
+							width = 10,
+							length = 20,
+							height = 6,
+							doors = []
+						}
+					}
+				]
+			}
+		},
+		{
+			wall = "SouthWall",
+			left = 2,
+			width = 1,
+			height = 2,
+			room = {
+				name = "South Room",
+				width = 40,
+				length = 16,
+				height = 4,
+				doors = []
+			}
+		},
+		{
+			wall = "WestWall",
+			left = 2,
+			width = 1,
+			height = 2,
+			room = {
+				name = "West Room",
+				width = 20,
+				length = 10,
+				height = 4,
+				doors = []
+			}
+		},
+		{
+			wall = "EastWall",
+			left = 2,
+			width = 1,
+			height = 2,
+			room = {
+				name = "East Room",
+				width = 12,
+				length = 10,
+				height = 6,
+				doors = []
+			}
+		}
+	]
+}
+
+func create_room_from_map (room_spec):
+	if ROOM_LIST.has(room_spec.name):
+		return ROOM_LIST[room_spec.name]
+
+	var room = room_scene.instance()
+	room.init(room_spec.width, room_spec.length, room_spec.height, room_spec.name)
+
+	for door_spec in room_spec.doors:
+		var door = room.get_node(door_spec.wall).add_door(
+			door_spec.left, door_spec.width, door_spec.height, door_spec.room.name, true)
+
+		door.get_node("DoorBody").connect("try_to_open", self, "_on_Door_try_to_open",
+			[room_spec, door_spec, room])
+
+	ROOM_LIST[room_spec.name] = room
+	return room
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-	var start_room = room_scene.instance()
-	var north_room = room_scene.instance()
-	var south_room = room_scene.instance()
-	var west_room = room_scene.instance()
-	var east_room = room_scene.instance()
-	var north_west_room = room_scene.instance()
-
-	start_room.init(10, 10, 4, "Central Room")
-	north_room.init(20, 20, 4, "North Room")
-	south_room.init(40, 16, 4, "South Room")
-	west_room.init(20, 10, 4, "West Room")
-	east_room.init(12, 10, 6, "East Room")
-	north_west_room.init(10, 20, 6, "North-West Room")
-
-	attach_room(start_room, north_room, "NorthWall", 2, 1, 2)
-	attach_room(start_room, south_room, "SouthWall", 2, 1, 2)
-	attach_room(start_room, west_room, "WestWall", 5, 1, 2)
-	attach_room(start_room, east_room, "EastWall", 5, 1, 2)
-	attach_room(north_room, north_west_room, "WestWall", 5, 1, 2)
-
+	var start_room = create_room_from_map(ROOM_MAP)
 	add_child(start_room)
-	#add_child(north_room)
-	#add_child(south_room)
-	#add_child(west_room)
-	#add_child(east_room)
-	#add_child(north_west_room)
 
-func _on_Door_try_to_open (doorBody, room1, room2, room1_wall_name, door_left, door_width, door_height):
+func _on_Door_try_to_open (door_body, room1_spec, door_spec, room1):
 	print('open door handler')
 
-	var room2_wall = room2.get_node(OPPOSING_WALL_MAP[room1_wall_name])
-	room2_wall.add_door(room2_wall.wall_width / 2.0, door_width, door_height, room1.room_name, false)
+	var room2 = create_room_from_map(door_spec.room)
+	var room2_wall = room2.get_node(OPPOSING_WALL_MAP[door_spec.wall])
+	room2_wall.add_door(room2_wall.wall_width / 2.0,
+			door_spec.width, door_spec.height, room1_spec.name, false)
 
 	var room2_offset
-	if room1_wall_name == "NorthWall":
+	if door_spec.wall == "NorthWall":
 		room2_offset = Vector3(
-			door_left - room1.room_width / 2.0,
+			door_spec.left - room1_spec.width / 2.0,
 			0,
-			-room2.room_length / 2.0 - room1.room_length / 2.0 - 2 * THICKNESS
+			-room2.room_length / 2.0 - room1_spec.length / 2.0 - 2 * THICKNESS
 		)
-	elif room1_wall_name == "SouthWall":
+	elif door_spec.wall == "SouthWall":
 		room2_offset = Vector3(
-			door_left - room1.room_width / 2.0,
+			door_spec.left - room1_spec.width / 2.0,
 			0,
-			room2.room_length / 2.0 + room1.room_length / 2.0 + 2 * THICKNESS
+			room2.room_length / 2.0 + room1_spec.length / 2.0 + 2 * THICKNESS
 		)
-	elif room1_wall_name == "WestWall":
+	elif door_spec.wall == "WestWall":
 		room2_offset = Vector3(
-			-room2.room_width / 2.0 - room1.room_width / 2.0 - 2 * THICKNESS,
+			-room2.room_width / 2.0 - room1_spec.width / 2.0 - 2 * THICKNESS,
 			0,
-			door_left - room1.room_length / 2.0
+			door_spec.left - room1_spec.length / 2.0
 		)
 	else: # EastWall
 		room2_offset = Vector3(
-			room2.room_width / 2.0 + room1.room_width / 2.0 + 2 * THICKNESS,
+			room2.room_width / 2.0 + room1_spec.width / 2.0 + 2 * THICKNESS,
 			0,
-			door_left - room1.room_length / 2.0
+			door_spec.left - room1_spec.length / 2.0
 		)
 
 	room2.set_translation(room1.get_translation() + room2_offset)
 	add_child(room2)
-	doorBody.open()
-
-func attach_room (room1, room2, room1_wall_name, door_left, door_width, door_height):
-	var door = room1.get_node(room1_wall_name).add_door(door_left, door_width, door_height, room2.room_name, true)
-
-	door.get_node("DoorBody").connect("try_to_open", self, "_on_Door_try_to_open",
-		[room1, room2, room1_wall_name, door_left, door_width, door_height])
+	door_body.open()
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
