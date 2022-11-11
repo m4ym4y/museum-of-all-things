b297fb445753197e7e993f00277f267be1685b5c
m4ym4y <m4ym4y@users.noreply.github.com>
2022-11-10 16:53:39 -0800

    commit change too old for me to remember

--- RoomGenerator.gd
+++ RoomGenerator.gd
@@ -23,6 +23,7 @@ var linked_pages
 var linked_pages_complete = false
 var linked_pages_original_request
 var linked_request_limit
+var fetching_room_name
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -38,6 +39,8 @@ func _ready():
 	$RelatedHTTPRequest.connect("request_completed", self, "_on_related_request_completed")
 
 func fetch_room(room_name):
+	print("fetch room ", room_name)
+	fetching_room_name = room_name
 	summary_response = null
 	linked_pages = []
 	linked_pages_complete = false
@@ -50,22 +53,29 @@ func fetch_room(room_name):
 func _on_summary_request_completed(result, response_code, headers, body):
 	summary_response = JSON.parse(body.get_string_from_utf8()).result
 	if linked_pages_complete:
+		print("room ready from summary ", summary_response == null)
+		if summary_response == null:
+			print("invalid JSON response: ", response_code, " ", body.get_string_from_utf8())
 		_on_room_ready()
 
 func _on_related_request_completed(result, response_code, headers, body):
 	var response_batch = JSON.parse(body.get_string_from_utf8()).result
 
-	print('got response', response_batch)
+	if not response_batch:
+		print("error with response batch: ", body.get_string_from_utf8())
+	
 	for page in response_batch.query.pages:
-		linked_pages += response_batch.query.pages[page].links
+		if page != "-1":
+			linked_pages += response_batch.query.pages[page].links
 
 	if response_batch.has('continue') and linked_request_limit > 0:
 		linked_request_limit -= 1
 		$RelatedHTTPRequest.request(linked_pages_original_request +
-				"&plcontinue=" + response_batch.continue.plcontinue, COMMON_HEADERS)
+				"&plcontinue=" + response_batch.continue.plcontinue.percent_encode(), COMMON_HEADERS)
 	else:
 		linked_pages_complete = true
-		if summary_response:
+		if summary_response != null:
+			print('room ready ', summary_response == null)
 			_on_room_ready()
 
 func _on_room_ready():
@@ -83,8 +93,12 @@ func _on_room_ready():
 		}
 		wall_index += 1
 
+	if fetching_room_name != summary_response.title:
+		print("got mismatched room names: original=" + fetching_room_name +
+				" got=" + summary_response.title)
+
 	var room_spec = {
-		name = summary_response.title,
+		name = fetching_room_name,
 		width = 24,
 		height = 4,
 		length = 24,
