e24f2162b3a34de0aa0e06ff3c5ba6f93b6c1806
m4ym4y <m4ym4y@users.noreply.github.com>
2023-03-12 15:08:55 -0700

    godot keeps changing the tabbing in exhibit.gd...

--- Museum.gd
+++ Museum.gd
@@ -86,15 +86,28 @@ const static_exhibit_data = {
 
 var loaded_exhibits = {}
 
-func load_exhibit(title):
+func load_exhibit(title, translation = Vector3(0, 0, 0), angle = 0):
 	var exhibit = Exhibit.instance()
 
 	# TODO: load dynamically from wikipedia
 	exhibit.init(static_exhibit_data[title])
 	loaded_exhibits[title] = exhibit
 
+	exhibit.connect("open_door", self, "_on_open_door", [title])
+	exhibit.rotation.y = angle
+	exhibit.translation = translation - exhibit.entrance
+
 	# TODO: position exhibit at the correct location according to the triggered door
 	add_child(exhibit)
+
+func _on_open_door(to_exhibit, door_translation, door_angle, from_exhibit):
+	print("HANDLING OPEN DOOR")
+	# Free exhibits other than the one we're in
+	for k in loaded_exhibits.keys():
+		if k != from_exhibit:
+			loaded_exhibits[k].queue_free()
+
+	load_exhibit(to_exhibit, door_translation, door_angle)
 
 func _ready():
 	load_exhibit(starting_exhibit)
