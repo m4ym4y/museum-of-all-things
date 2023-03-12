5d6e028b3333ee36ef2220405f909690fe540d62
m4ym4y <m4ym4y@users.noreply.github.com>
2023-03-11 18:53:20 -0800

    add image items to the exhibit

--- Exhibit.gd
+++ Exhibit.gd
@@ -1,8 +1,11 @@
 extends Spatial
 
 const room_types = [
-	preload("res://room_scenes/Hallway1.tscn")
+	preload("res://room_scenes/Hallway1.tscn"),
+	preload("res://room_scenes/Hallway2.tscn")
 ]
+
+const GallerySprite = preload("res://room_items/GallerySprite.tscn")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,7 +14,43 @@ const room_types = [
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# in future this will be init with wikipedia article
-	init(['hello', 'world', 'test', 'lorem', 'ipsum'])
+	"""init(['hello', 'world', 'test', 'lorem', 'ipsum',
+		'hello', 'world', 'test', 'lorem', 'ipsum',
+		'hello', 'world', 'test', 'lorem', 'ipsum',
+		'hello', 'world', 'test', 'lorem', 'ipsum',
+		'hello', 'world', 'test', 'lorem', 'ipsum',
+		'hello', 'world', 'test', 'lorem', 'ipsum'
+	])"""
+	init([
+		{
+			"type": "image",
+			"src": "//upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/640px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg"
+		},
+		{
+			"type": "image",
+			"src": "//upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Albert_Einstein_at_the_age_of_three_%281882%29.jpg/320px-Albert_Einstein_at_the_age_of_three_%281882%29.jpg"
+		},
+		{
+			"type": "image",
+			"src": "//upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Albert_Einstein_as_a_child.jpg/640px-Albert_Einstein_as_a_child.jpg"
+		},
+		{
+			"type": "image",
+			"src": "//upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Albert_Einstein%27s_exam_of_maturity_grades_%28color2%29.jpg/640px-Albert_Einstein%27s_exam_of_maturity_grades_%28color2%29.jpg"
+		},
+		{
+			"type": "image",
+			"src": "//upload.wikimedia.org/wikipedia/commons/thumb/8/87/Albert_Einstein_and_his_wife_Mileva_Maric.jpg/640px-Albert_Einstein_and_his_wife_Mileva_Maric.jpg"
+		},
+		{
+			"type": "image",
+			"src": "//upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Einstein_patentoffice.jpg/640px-Einstein_patentoffice.jpg"
+		},
+		{
+			"type": "image",
+			"src": "//upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Einstein_thesis.png/640px-Einstein_thesis.png"
+		},
+	])
 
 # TODO: put this in main class instead
 func _input(event):
@@ -24,18 +63,24 @@ func _input(event):
 func add_room():
 	pass
 
+func get_angle(child):
+	if child.properties.has("angle"):
+		return float(child.properties.angle) * 2.0 * PI / 360.0
+	else:
+		return 0.0
+
 # start with items that are just text
 func init(items):
 	var exit_pos = Vector3(0, 0, 0)
 
 	while items.size() > 0:
 		# todo: choose room randomly
-		var room = room_types[0].instance()
+		var room = room_types[1].instance()
 		var entrance_pos = Vector3(0, 0, 0)
 		var next_exit_pos
 
 		# add as many items as can fit in the room
-		for child in room.get_children():
+		for child in room.get_node("map").get_children():
 			if child.name.ends_with("entrance"):
 				entrance_pos = child.translation
 				print("found entrance: ", entrance_pos)
@@ -47,11 +92,16 @@ func init(items):
 			elif child.name.ends_with("item"):
 				print("found item")
 				var item = items.pop_front()
+				if not item:
+					continue
 
-				var item_scene = Label3D.new()
+				"""var item_scene = Label3D.new()
 				item_scene.text = item
-				item_scene.rotation.y = float(child.properties.angle) * 2.0 * PI / 360.0
-				print('angle:', float(child.properties.angle) * 2.0 * PI / 360.0)
+				item_scene.rotation.y = get_angle(child)
+				print('angle:', get_angle(child))"""
+
+				var item_scene = GallerySprite.instance()
+				item_scene.init(item.src, 1, 1)
 
 				child.add_child(item_scene)
 
---
+++ room_items/GallerySprite.gd
@@ -0,0 +1,56 @@
+extends Sprite3D
+
+var image_url
+var PNG_REGEX = RegEx.new()
+var JPG_REGEX = RegEx.new()
+var width
+var height
+
+const PIXELS_PER_METER = 200
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func _on_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_error("Failed to fetch the image at " + image_url)
+		return
+
+	print("LOADED IMAGE ", image_url, " ", response_code)
+	var image_texture = ImageTexture.new()
+	var image = Image.new()
+
+	if JPG_REGEX.search(image_url.to_lower()):
+		image.load_jpg_from_buffer(body)
+	elif PNG_REGEX.search(image_url.to_lower()):
+		image.load_png_from_buffer(body)
+
+	image.resize(width * PIXELS_PER_METER, height * PIXELS_PER_METER,
+			Image.INTERPOLATE_NEAREST)
+
+	image_texture.create_from_image(image)
+	texture = image_texture
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	PNG_REGEX.compile("\\.png$")
+	JPG_REGEX.compile("\\.(jpg|jpeg)$")
+	if image_url:
+		$HTTPRequest.request(image_url)
+	pass
+
+func init(url, _width, _height):
+	print("LOADING IMAGE BY URL ", url)
+	if url.begins_with('//'):
+		image_url = 'https:' + url
+	else:
+		image_url = url
+	width = _width
+	height = _height
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
+	if is_inside_tree():
+		$HTTPRequest.request(image_url)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
