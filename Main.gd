ce4df624138f339a58b5b52aa50f359c5a1230e4
m4ym4y <m4ym4y@users.noreply.github.com>
2023-03-19 22:23:43 -0700

    make the lighting white and brighter

--- Main.gd
+++ Main.gd
@@ -8,6 +8,8 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if OS.get_name() != "HTML5":
+		$WorldEnvironment.environment.ss_reflections_enabled = true
 	$Timer.connect("timeout", self, "_on_timer_timeout")
 
 func _on_timer_timeout():
