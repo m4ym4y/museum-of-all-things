5584a6a2217a0c989044304484e91e73d7ec0739
m4ym4y <m4ym4y@users.noreply.github.com>
2024-11-18 23:09:42 -0800

    more tiled exhibit generation w/ exit halls

--- tilemaptest.gd
+++ tilemaptest.gd
@@ -3,7 +3,21 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+  $TiledExhibitGenerator.generate(
+      min_room_width,
+      max_room_width,
+      min_room_length,
+      max_room_length,
+      room_count
+  )
   pass # Replace with function body.
+
+func _input(event):
+  if event.is_action_pressed("ui_cancel"):
+    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+  if event.is_action_pressed("click"):
+    if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+      Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta: float) -> void:
   pass
