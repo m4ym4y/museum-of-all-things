3a234e18f551753886cd2ee6a39e7df1df370a99
m4ym4y <m4ym4y@users.noreply.github.com>
2024-11-26 01:31:18 -0800

    add VR support, albeit a little janky

--- scenes/util/Util.gd
+++ scenes/util/Util.gd
@@ -1,27 +1,30 @@
 extends Node
 
 func vecToRot(vec):
-  if vec.z < -0.1:
-    return 0.0
-  elif vec.z > 0.1:
-    return PI
-  elif vec.x > 0.1:
-    return 3 * PI / 2
-  elif vec.x < -0.1:
-    return PI / 2
-  return 0.0
+	if vec.z < -0.1:
+		return 0.0
+	elif vec.z > 0.1:
+		return PI
+	elif vec.x > 0.1:
+		return 3 * PI / 2
+	elif vec.x < -0.1:
+		return PI / 2
+	return 0.0
 
 func vecToOrientation(grid, vec):
-  var vec_basis = Basis.looking_at(vec.normalized())
-  return grid.get_orthogonal_index_from_basis(vec_basis)
+	var vec_basis = Basis.looking_at(vec.normalized())
+	return grid.get_orthogonal_index_from_basis(vec_basis)
 
 func gridToWorld(vec):
-  return 4 * vec
+	return 4 * vec
 
 func coalesce(a, b):
-  return a if a else b
+	return a if a else b
 
 func clear_listeners(n, sig_name):
-  var list = n.get_signal_connection_list(sig_name)
-  for c in list:
-    c.signal.disconnect(c.callable)
+	var list = n.get_signal_connection_list(sig_name)
+	for c in list:
+		c.signal.disconnect(c.callable)
+
+func is_xr():
+	return ProjectSettings.get_setting("xr/openxr/enabled")
