5887c3ff66c2e12cee059df999382bf2e0f079a2
m4ym4y <m4ym4y@users.noreply.github.com>
2024-12-08 15:58:06 -0800

    add wikimedia commons images to exhibits!!!!

--- scenes/util/ExhibitFetcher.gd
+++ scenes/util/ExhibitFetcher.gd
@@ -2,19 +2,27 @@ extends Node
 
 signal wikitext_complete(titles, context)
 signal wikitext_failed(titles, message)
+signal wikidata_complete(ids, context)
 signal images_complete(files, context)
+signal commons_images_complete(category, context)
 
 const MAX_BATCH_SIZE = 50
 const REQUEST_DELAY = 1.0
 const USER_AGENT = "https://github.com/m4ym4y/wikipedia-museum"
 
 # TODO: wikimedia support, and category support
-const WIKIMEDIA_PREFIX = "https://commons.wikimedia.org/wiki/"
+const WIKIMEDIA_COMMONS_PREFIX = "https://commons.wikimedia.org/wiki/"
 const WIKIPEDIA_PREFIX = "https://wikipedia.org/wiki/"
+const WIKIDATA_PREFIX = "https://www.wikidata.org/wiki/"
+
+const WIKIDATA_COMMONS_CATEGORY = "P373"
+
 var COMMON_HEADERS
 
-var wikitext_endpoint = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions|extracts&explaintext=true&rvprop=content&format=json&redirects=1&titles="
+var wikitext_endpoint = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions|extracts|pageprops&ppprop=wikibase_item&explaintext=true&rvprop=content&format=json&redirects=1&titles="
 var images_endpoint = "https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata|url&iiurlwidth=640&iiextmetadatafilter=LicenseShortName|Artist&format=json&redirects=1&titles="
+var wikidata_endpoint = "https://www.wikidata.org/w/api.php?action=wbgetclaims&property=P373&format=json&entity="
+var wikimedia_commons_images_endpoint = "https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmlimit=max&prop=imageinfo&iiprop=url|extmetadata&iiurlwidth=640&iiextmetadatafilter=Artist|LicenseShortName&format=json&gcmtitle="
 
 var _request_queue = []
 var _request_in_flight = false
@@ -39,27 +47,31 @@ func _advance_queue():
 	if next_fetch != null:
 		if next_fetch[0] == "fetch_wikitext":
 			fetch(next_fetch[1], next_fetch[2])
+		if next_fetch[0] == "fetch_wikidata":
+			fetch_wikidata(next_fetch[1], next_fetch[2])
 		elif next_fetch[0] == "fetch_images":
 			fetch_images(next_fetch[1], next_fetch[2])
+		elif next_fetch[0] == "fetch_commons_images":
+			fetch_commons_images(next_fetch[1], next_fetch[2])
 		else:
 			push_error("unknown queue item. type=", next_fetch[0])
 
 func _join_titles(titles):
 	return "|".join(titles.map(func(t): return t.uri_encode()))
 
-func _read_from_cache(title):
-	var json = DataManager.load_json_data(WIKIPEDIA_PREFIX + title)
+func _read_from_cache(title, prefix=WIKIPEDIA_PREFIX):
+	var json = DataManager.load_json_data(prefix + title)
 	if json:
 		_results[title] = json
 	return json
 
-func _get_uncached_titles(titles):
+func _get_uncached_titles(titles, prefix=WIKIPEDIA_PREFIX):
 	var new_titles = []
 	for title in titles:
 		if title == "":
 			continue
 		if not get_result(title):
-			var cached = _read_from_cache(title)
+			var cached = _read_from_cache(title, prefix)
 			if not cached:
 				new_titles.append(title)
 	return new_titles
@@ -85,6 +97,47 @@ func fetch_images(files, context):
 	var ctx = {
 		"files": files,
 		"new_files": new_files,
+	}
+
+	_dispatch_request(url, ctx, context)
+
+func fetch_commons_images(category, context):
+	var new_category = _get_uncached_titles([category], WIKIMEDIA_COMMONS_PREFIX)
+
+	if len(new_category) == 0:
+		emit_signal("commons_images_complete", category, context)
+		return
+
+	# queue if another request is in flight
+	if _request_in_flight:
+		_request_queue.append([ "fetch_commons_images", category, context ])
+		return
+	_request_in_flight = true
+
+	var url = wikimedia_commons_images_endpoint + category.uri_encode()
+	var ctx = {
+		"category": category,
+		"image_titles": []
+	}
+
+	_dispatch_request(url, ctx, context)
+
+func fetch_wikidata(entity, context):
+	var new_entity = _get_uncached_titles([entity], WIKIDATA_PREFIX)
+
+	if len(new_entity) == 0:
+		emit_signal("wikidata_complete", entity, context)
+		return
+
+	# queue if another request is in flight
+	if _request_in_flight:
+		_request_queue.append([ "fetch_wikidata", entity, context ])
+		return
+	_request_in_flight = true
+
+	var url = wikidata_endpoint + entity.uri_encode()
+	var ctx = {
+		"entity": entity
 	}
 
 	_dispatch_request(url, ctx, context)
@@ -166,13 +219,6 @@ func _filter_links_ns(links):
 			agg.append(link.title)
 	return agg
 
-func _filter_extlinks(links):
-	var agg = []
-	for link in links:
-		if link.has("*") and link["*"].begins_with(WIKIMEDIA_PREFIX):
-			agg.append(link["*"])
-	return agg
-
 func _normalize_article_title(title):
 	var new_title = title.replace("_", " ").uri_decode()
 	var title_fragments = new_title.split("#")
@@ -192,39 +238,53 @@ func _on_request_completed(result, response_code, headers, body, ctx, caller_ctx
 
 	var res = _get_json(body)
 
-	if not res.has("query"):
+	if res.has("query"):
+		var query = res.query
+
+		# handle the canonical names
+		if query.has("normalized"):
+			var normalized = query.normalized
+			for title in normalized:
+				_set_page_field(title.from, "normalized", title.to)
+
+		if query.has("redirects"):
+			var redirects = query.redirects
+			for title in redirects:
+				_set_page_field(title.from, "normalized", title.to)
+
+	# wikipedia request must have "query" object.
+	# wikidata does not need to have it
+	elif not ctx.url.begins_with(wikidata_endpoint):
+		print("RETURN TRUE BC NO QUERY")
 		return true
-
-	var query = res.query
-
-	# handle the canonical names
-	if query.has("normalized"):
-		var normalized = query.normalized
-		for title in normalized:
-			_set_page_field(title.from, "normalized", title.to)
-
-	if query.has("redirects"):
-		var redirects = query.redirects
-		for title in redirects:
-			_set_page_field(title.from, "normalized", title.to)
 
 	if ctx.url.begins_with(wikitext_endpoint):
 		return _on_wikitext_request_complete(res, ctx, caller_ctx)
 	elif ctx.url.begins_with(images_endpoint):
 		return _on_images_request_complete(res, ctx, caller_ctx)
+	elif ctx.url.begins_with(wikidata_endpoint):
+		print("DISPATCH ON WIKIDATA REQUEST COMPLETE")
+		return _on_wikidata_request_complete(res, ctx, caller_ctx)
+	elif ctx.url.begins_with(wikimedia_commons_images_endpoint):
+		return _on_commons_images_request_complete(res, ctx, caller_ctx)
 
 func _dispatch_continue(continue_fields, base_url, titles, ctx, caller_ctx):
-	var continue_url = base_url + _join_titles(titles)
+	var continue_url = base_url
+	if typeof(titles) == TYPE_ARRAY:
+		continue_url += _join_titles(titles)
+	else:
+		continue_url += titles.uri_encode()
+
 	for field in continue_fields.keys():
 		continue_url += "&" + field + "=" + continue_fields[field].uri_encode()
 	ctx.url = continue_url
 	_dispatch_request(continue_url, ctx, caller_ctx)
 
-func _cache_all(titles):
+func _cache_all(titles, prefix=WIKIPEDIA_PREFIX):
 	for title in titles:
 		var result = get_result(title)
 		if result != null:
-			DataManager.save_json_data(WIKIPEDIA_PREFIX + title, result)
+			DataManager.save_json_data(prefix + title, result)
 
 func _on_wikitext_request_complete(res, ctx, caller_ctx):
 	# store the information we did get
@@ -237,6 +297,9 @@ func _on_wikitext_request_complete(res, ctx, caller_ctx):
 				_set_page_field(page.title, "wikitext", revisions[0]["*"])
 			if page.has("extract"):
 				_set_page_field(page.title, "extract", page.extract)
+			if page.has("pageprops") and page.pageprops.has("wikibase_item"):
+				var item = page.pageprops.wikibase_item
+				_set_page_field(page.title, "wikidata_entity", item)
 
 	# handle continues
 	if res.has("continue"):
@@ -274,3 +337,48 @@ func _on_images_request_complete(res, ctx, caller_ctx):
 		_cache_all(ctx.new_files)
 		emit_signal("images_complete", ctx.files, caller_ctx)
 		return true
+
+func _on_commons_images_request_complete(res, ctx, caller_ctx):
+	var image_titles = ctx.image_titles
+	if res.query.has("pages"):
+		var pages = res.query.pages
+		for page_id in pages.keys():
+			var page = pages[page_id]
+			var file = page.title
+			if not page.has("imageinfo"):
+				continue
+			for info in page.imageinfo:
+				if info.has("extmetadata"):
+					var md = info.extmetadata
+					if md.has("LicenseShortName"):
+						_set_page_field(file, "license_short_name", md.LicenseShortName.value)
+					if md.has("Artist"):
+						_set_page_field(file, "artist", md.Artist.value)
+				if info.has("thumburl"):
+					_set_page_field(file, "src", info.thumburl)
+				image_titles.append(file)
+				_append_page_field(ctx.category, "images", [ file ])
+
+	# handle continues
+	if res.has("continue"):
+		_dispatch_continue(res.continue, wikimedia_commons_images_endpoint, ctx.category, ctx, caller_ctx)
+		return false
+	else:
+		_cache_all(ctx.image_titles, WIKIMEDIA_COMMONS_PREFIX)
+		_cache_all([ ctx.category ], WIKIMEDIA_COMMONS_PREFIX)
+		emit_signal("commons_images_complete", ctx.category, caller_ctx)
+		return true
+
+func _on_wikidata_request_complete(res, ctx, caller_ctx):
+	# store the information we did get
+	if res.has("claims"):
+		if res.claims.has(WIKIDATA_COMMONS_CATEGORY):
+			var claims = res.claims[WIKIDATA_COMMONS_CATEGORY]
+			if len(claims) > 0:
+				var claim = claims[0]
+				var value = claim.mainsnak.datavalue.value
+				_set_page_field(ctx.entity, "commons_category", "Category:" + value)
+
+	_cache_all([ ctx.entity ], WIKIDATA_PREFIX)
+	emit_signal("wikidata_complete", ctx.entity, caller_ctx)
+	return true
