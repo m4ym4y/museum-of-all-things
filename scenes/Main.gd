1119e9f16755f83ec2426213dacdad4225ac1718
m4ym4y <m4ym4y@users.noreply.github.com>
2024-12-12 00:07:24 -0800

    fps counter

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -58,4 +58,4 @@ func _on_button_pressed():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-  pass
+  $FpsLabel.text = str(Engine.get_frames_per_second())
