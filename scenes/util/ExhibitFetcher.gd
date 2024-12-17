3cc49641634d9c79654d2e1678c0c8d5cf39e259
m4ym4y <m4ym4y@users.noreply.github.com>
2024-12-16 22:59:39 -0800

    fetch from wikipedia in a separate thread

--- scenes/util/ExhibitFetcher.gd
+++ scenes/util/ExhibitFetcher.gd
@@ -8,7 +8,6 @@ signal commons_images_complete(category, context)
 
 const MAX_BATCH_SIZE = 50
 const REQUEST_DELAY = 1.0
-const USER_AGENT = "https://github.com/m4ym4y/wikipedia-museum"
 
 # TODO: wikimedia support, and category support
 const WIKIMEDIA_COMMONS_PREFIX = "https://commons.wikimedia.org/wiki/"
@@ -17,23 +16,51 @@ const WIKIDATA_PREFIX = "https://www.wikidata.org/wiki/"
 
 const WIKIDATA_COMMONS_CATEGORY = "P373"
 
-var COMMON_HEADERS
-
 var wikitext_endpoint = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions|extracts|pageprops&ppprop=wikibase_item&explaintext=true&rvprop=content&format=json&redirects=1&titles="
 var images_endpoint = "https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata|url&iiurlwidth=640&iiextmetadatafilter=LicenseShortName|Artist&format=json&redirects=1&titles="
 var wikidata_endpoint = "https://www.wikidata.org/w/api.php?action=wbgetclaims&property=P373&format=json&entity="
 var wikimedia_commons_images_endpoint = "https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmlimit=max&prop=imageinfo&iiprop=url|extmetadata&iiurlwidth=640&iiextmetadatafilter=Artist|LicenseShortName&format=json&gcmtitle="
 
+var _request_queue_lock = Mutex.new()
 var _request_queue_map = {}
 var _request_queue_title = "$Lobby"
-var _request_in_flight = false
+
+var _fs_lock = Mutex.new()
+var _results_lock = Mutex.new()
 var _results = {}
 
+var _network_request_thread = Thread.new()
+var NETWORK_QUEUE = "Network"
+
 func _ready():
-	if OS.get_name() != "HTML5":
-		COMMON_HEADERS = [ "accept: application/json; charset=utf-8", "user-agent: " + USER_AGENT ]
-	else:
-		COMMON_HEADERS = [ "accept: application/json; charset=utf-8" ]
+	_network_request_thread.start(_network_request_thread_loop)
+
+func _delayed_advance_queue():
+	get_tree().create_timer(REQUEST_DELAY).timeout.connect(_advance_queue)
+
+func _network_request_thread_loop():
+	while true:
+		var item = WorkQueue.process_queue(NETWORK_QUEUE)
+		if item[0] == "fetch_wikitext":
+			_fetch_wikitext(item[1], item[2])
+		elif item[0] == "fetch_images":
+			_fetch_images(item[1], item[2])
+		elif item[0] == "fetch_commons_images":
+			_fetch_commons_images(item[1], item[2])
+		elif item[0] == "fetch_wikidata":
+			_fetch_wikidata(item[1], item[2])
+
+func fetch(titles, ctx):
+	WorkQueue.add_item(NETWORK_QUEUE, ["fetch_wikitext", titles, ctx])
+
+func fetch_images(titles, ctx):
+	WorkQueue.add_item(NETWORK_QUEUE, ["fetch_images", titles, ctx])
+
+func fetch_wikidata(titles, ctx):
+	WorkQueue.add_item(NETWORK_QUEUE, ["fetch_wikidata", titles, ctx])
+
+func fetch_commons_images(titles, ctx):
+	WorkQueue.add_item(NETWORK_QUEUE, ["fetch_commons_images", titles, ctx])
 
 const LOCATION_STR = "location: "
 func _get_location_header(headers):
@@ -45,13 +72,13 @@ func switch_active_queue(title):
 	if not _request_queue_map.has(title):
 		_request_queue_map[title] = []
 	_request_queue_title = title
-	if not _request_in_flight:
-		_advance_queue()
+	_advance_queue()
 
 # TODO: should the queue batch together all the upcoming requests?
 func _advance_queue():
-	_request_in_flight = false
+	_request_queue_lock.lock()
 	var next_fetch = _request_queue_map[_request_queue_title].pop_front()
+	_request_queue_lock.lock()
 	if next_fetch != null:
 		next_fetch.call()
 
@@ -59,9 +86,13 @@ func _join_titles(titles):
 	return "|".join(titles.map(func(t): return t.uri_encode()))
 
 func _read_from_cache(title, prefix=WIKIPEDIA_PREFIX):
+	_fs_lock.lock()
 	var json = DataManager.load_json_data(prefix + title)
+	_fs_lock.unlock()
 	if json:
+		_results_lock.lock()
 		_results[title] = json
+		_results_lock.unlock()
 	return json
 
 func _get_uncached_titles(titles, prefix=WIKIPEDIA_PREFIX):
@@ -75,28 +106,21 @@ func _get_uncached_titles(titles, prefix=WIKIPEDIA_PREFIX):
 				new_titles.append(title)
 	return new_titles
 
-func fetch_images(files, context):
+func _fetch_images(files, context):
 	var new_files = _get_uncached_titles(files)
 
 	if len(new_files) == 0:
-		emit_signal("images_complete", files, context)
+		call_deferred("emit_signal", "images_complete", files, context)
 		return
 
 	if len(new_files) > MAX_BATCH_SIZE:
+		_request_queue_lock.lock()
 		_request_queue_map[_request_queue_title].append(fetch_images.bind(
 			new_files.slice(MAX_BATCH_SIZE),
 			context
 		))
+		_request_queue_lock.unlock()
 		new_files = new_files.slice(0, MAX_BATCH_SIZE)
-
-	# queue if another request is in flight
-	if _request_in_flight:
-		_request_queue_map[_request_queue_title].append(fetch_images.bind(
-			new_files,
-			context
-		))
-		return
-	_request_in_flight = true
 
 	var url = images_endpoint + _join_titles(new_files)
 	var ctx = {
@@ -107,7 +131,7 @@ func fetch_images(files, context):
 
 	_dispatch_request(url, ctx, context)
 
-func fetch_commons_images(category, context):
+func _fetch_commons_images(category, context):
 	var new_category = _get_uncached_titles([category], WIKIMEDIA_COMMONS_PREFIX)
 
 	if len(new_category) == 0:
@@ -115,17 +139,8 @@ func fetch_commons_images(category, context):
 		if result and result.has("images"):
 			for image in result.images:
 				_read_from_cache(image, WIKIMEDIA_COMMONS_PREFIX)
-			emit_signal("commons_images_complete", result.images, context)
+			call_deferred("emit_signal", "commons_images_complete", result.images, context)
 		return
-
-	# queue if another request is in flight
-	if _request_in_flight:
-		_request_queue_map[_request_queue_title].append(fetch_commons_images.bind(
-			category,
-			context
-		))
-		return
-	_request_in_flight = true
 
 	var url = wikimedia_commons_images_endpoint + category.uri_encode()
 	var ctx = {
@@ -135,18 +150,12 @@ func fetch_commons_images(category, context):
 
 	_dispatch_request(url, ctx, context)
 
-func fetch_wikidata(entity, context):
+func _fetch_wikidata(entity, context):
 	var new_entity = _get_uncached_titles([entity], WIKIDATA_PREFIX)
 
 	if len(new_entity) == 0:
-		emit_signal("wikidata_complete", entity, context)
+		call_deferred("emit_signal", "wikidata_complete", entity, context)
 		return
-
-	# queue if another request is in flight
-	if _request_in_flight:
-		_request_queue_map[_request_queue_title].append(fetch_wikidata.bind(entity, context))
-		return
-	_request_in_flight = true
 
 	var url = wikidata_endpoint + entity.uri_encode()
 	var ctx = {
@@ -155,11 +164,11 @@ func fetch_wikidata(entity, context):
 
 	_dispatch_request(url, ctx, context)
 
-func fetch(titles, context):
+func _fetch_wikitext(titles, context):
 	var new_titles = _get_uncached_titles(titles)
 
 	if len(new_titles) == 0:
-		emit_signal("wikitext_complete", titles, context)
+		call_deferred("emit_signal", "wikitext_complete", titles, context)
 		return
 
 	if len(new_titles) > MAX_BATCH_SIZE:
@@ -176,48 +185,48 @@ func fetch(titles, context):
 	_dispatch_request(url, ctx, context)
 
 func get_result(title):
+	var res = null
+	_results_lock.lock()
 	if _results.has(title):
 		var result = _results[title]
 		if result.has("normalized"):
 			if _results.has(result.normalized):
-				result = _results[result.normalized]
+				res = _results[result.normalized]
 			else:
-				return null
-		return result
-	else:
-		return null
+				res = null
+		else:
+			res = result
+	_results_lock.unlock()
+	return res
 
 func _dispatch_request(url, ctx, caller_ctx):
-	var request = HTTPRequest.new()
-	request.max_redirects = 0
-	request.request_completed.connect(
-		_on_request_completed_wrapper.bind(ctx, caller_ctx),
-		CONNECT_ONE_SHOT
-	)
-
-	add_child(request)
-	ctx.request = request
-	ctx.url = url
-
 	if OS.is_debug_build():
 		print("fetching url ", url)
-	var result = request.request(url, COMMON_HEADERS)
-	if result != OK:
-		push_error("failed to send http request ", result, " ", url)
-		request.queue_free()
-		get_tree().create_timer(REQUEST_DELAY).timeout.connect(_advance_queue)
+
+	ctx.url = url
+	var result = RequestSync.request(url)
+
+	if result[0] != OK:
+		push_error("failed to send http request ", result[0], " ", url)
+		call_deferred("_delayed_advance_queue")
+	else:
+		_on_request_completed_wrapper(result[0], result[1], result[2], result[3], ctx, caller_ctx)
 
 func _set_page_field(title, field, value):
+	_results_lock.lock()
 	if not _results.has(title):
 		_results[title] = {}
 	_results[title][field] = value
+	_results_lock.unlock()
 
 func _append_page_field(title, field, values):
+	_results_lock.lock()
 	if not _results.has(title):
 		_results[title] = {}
 	if not _results[title].has(field):
 		_results[title][field] = []
 	_results[title][field].append_array(values)
+	_results_lock.unlock()
 
 func _get_json(body):
 	var test_json_conv = JSON.new()
@@ -239,14 +248,14 @@ func _normalize_article_title(title):
 
 func _on_request_completed_wrapper(result, response_code, headers, body, ctx, caller_ctx):
 	if _on_request_completed(result, response_code, headers, body, ctx, caller_ctx):
-		get_tree().create_timer(REQUEST_DELAY).timeout.connect(_advance_queue)
+		call_deferred("_delayed_advance_queue")
 
 func _on_request_completed(result, response_code, headers, body, ctx, caller_ctx):
 	if result != 0 or response_code != 200:
 		if response_code != 404:
 			push_error("error in request ", result, " ", response_code, " ", ctx.url)
 		if ctx.url.begins_with(wikitext_endpoint):
-			emit_signal("wikitext_failed", ctx.new_titles, str(response_code))
+			call_deferred("emit_signal", "wikitext_failed", ctx.new_titles, str(response_code))
 		return true
 
 	var res = _get_json(body)
@@ -291,19 +300,23 @@ func _dispatch_continue(continue_fields, base_url, titles, ctx, caller_ctx):
 	ctx.url = continue_url
 
 	# continue right now if we're still the active article
-	var continue_call = _dispatch_request.bind(continue_url, ctx, caller_ctx)
 	if ctx.queue == _request_queue_title:
-		continue_call.call()
+		_dispatch_request(continue_url, ctx, caller_ctx)
 		return false
 	elif _request_queue_map.has(ctx.queue):
+		var continue_call = _dispatch_request.bind(continue_url, ctx, caller_ctx)
+		_request_queue_lock.lock()
 		_request_queue_map[ctx.queue].append(continue_call)
+		_request_queue_lock.unlock()
 		return true
 
 func _cache_all(titles, prefix=WIKIPEDIA_PREFIX):
 	for title in titles:
 		var result = get_result(title)
 		if result != null:
+			_fs_lock.lock()
 			DataManager.save_json_data(prefix + title, result)
+			_fs_lock.unlock()
 
 func _get_original_title(query, title):
 	if query.has("normalized"):
@@ -326,7 +339,7 @@ func _on_wikitext_request_complete(res, ctx, caller_ctx):
 			# emit failed signal for a missing page
 			if page.has("missing"):
 				var original_title = _get_original_title(res.query, page.title)
-				emit_signal("wikitext_failed", [original_title], "Missing")
+				call_deferred("emit_signal", "wikitext_failed", [original_title], "Missing")
 				ctx.new_titles.erase(original_title)
 				ctx.titles.erase(original_title)
 				continue
@@ -345,7 +358,7 @@ func _on_wikitext_request_complete(res, ctx, caller_ctx):
 		return _dispatch_continue(res.continue, wikitext_endpoint, ctx.new_titles, ctx, caller_ctx)
 	else:
 		_cache_all(ctx.new_titles)
-		emit_signal("wikitext_complete", ctx.titles, caller_ctx)
+		call_deferred("emit_signal", "wikitext_complete", ctx.titles, caller_ctx)
 		# wikitext ignores queue, so return false to prevent queue advance after completion
 		return false
 
@@ -373,7 +386,7 @@ func _on_images_request_complete(res, ctx, caller_ctx):
 
 	if len(file_batch) > 0:
 		_cache_all(file_batch)
-		emit_signal("images_complete", file_batch, caller_ctx)
+		call_deferred("emit_signal", "images_complete", file_batch, caller_ctx)
 
 	# handle continues
 	if res.has("continue"):
@@ -405,7 +418,7 @@ func _on_commons_images_request_complete(res, ctx, caller_ctx):
 
 	if len(file_batch) > 0:
 		_cache_all(file_batch)
-		emit_signal("commons_images_complete", file_batch, caller_ctx)
+		call_deferred("emit_signal", "commons_images_complete", file_batch, caller_ctx)
 
 	# handle continues
 	if res.has("continue"):
@@ -425,5 +438,5 @@ func _on_wikidata_request_complete(res, ctx, caller_ctx):
 				_set_page_field(ctx.entity, "commons_category", "Category:" + value)
 
 	_cache_all([ ctx.entity ], WIKIDATA_PREFIX)
-	emit_signal("wikidata_complete", ctx.entity, caller_ctx)
+	call_deferred("emit_signal", "wikidata_complete", ctx.entity, caller_ctx)
 	return true
---
+++ scenes/util/RequestSync.gd
@@ -0,0 +1,52 @@
+extends Node
+
+var port = 443
+var protocol = "https://"
+var DELAY_MS = 10
+var USER_AGENT = "https://github.com/m4ym4y/wikipedia-museum"
+var COMMON_HEADERS = [
+	"accept: application/json; charset=utf-8",
+	"user-agent: " + USER_AGENT
+]
+
+# TODO: a version where we reuse the http client
+func request(url, json=true):
+	var http_client = HTTPClient.new()
+	var host_idx = url.find("/", len(protocol)) # first slash after protocol
+	var host = url.substr(0, host_idx)
+	var path = url.substr(host_idx)
+
+	var err = http_client.connect_to_host(host, port, TLSOptions.client())
+	if err != OK:
+		return [err, 0, null]
+
+	while (
+		http_client.get_status() == HTTPClient.STATUS_CONNECTING or
+		http_client.get_status() == HTTPClient.STATUS_RESOLVING
+	):
+		http_client.poll()
+		OS.delay_msec(DELAY_MS)
+
+	if http_client.get_status() != HTTPClient.STATUS_CONNECTED:
+		return [FAILED, 0, null, null]
+
+	http_client.request(HTTPClient.METHOD_GET, path, COMMON_HEADERS)
+
+	while http_client.get_status() == HTTPClient.STATUS_REQUESTING:
+		http_client.poll()
+		OS.delay_msec(DELAY_MS)
+
+	if http_client.get_status() != HTTPClient.STATUS_BODY:
+		return [FAILED, 0, null, null]
+
+	var response = PackedByteArray()
+	while http_client.get_status() == HTTPClient.STATUS_BODY:
+		http_client.poll()
+		var chunk = http_client.read_response_body_chunk()
+		if chunk.size() > 0:
+			response.append_array(chunk)
+		OS.delay_msec(DELAY_MS)
+
+	var response_code = http_client.get_response_code()
+	var response_headers = http_client.get_response_headers()
+	return [OK, response_code, response_headers, response]
