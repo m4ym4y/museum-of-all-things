0215514cf8d4bcf13ea8d99f3dd508301aadae28
m4ym4y <m4ym4y@users.noreply.github.com>
2024-12-17 20:38:20 -0800

    auto trim height of rich text elements

--- scenes/items/RichTextItem.gd
+++ scenes/items/RichTextItem.gd
@@ -1,8 +1,5 @@
 extends Node3D
 
-#static var max_chars = 2500
-#static var max_chars = 1500
-static var max_chars = 1000
 static var margin_top = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,7 +8,7 @@ func _process(delta: float) -> void:
 
 func init(text):
   var label = $SubViewport/Control/RichTextLabel
-  var t = Util.trim_to_length_sentence(Util.strip_markup(text), max_chars)
+  var t = Util.strip_markup(text)
   label.text = t
   call_deferred("_center_vertically", label)
 
@@ -21,6 +18,13 @@ func _center_vertically(label):
 
   # Get the content height from the RichTextLabel
   var content_height = label.get_content_height()
+
+  if content_height > viewport_size.y - 2 * margin_top:
+    var text_len = len(label.text)
+    var new_len = text_len * (float(content_height - 2 * margin_top) / float(content_height))
+    label.text = Util.trim_to_length_sentence(label.text, min(text_len - 1, new_len))
+    call_deferred("_center_vertically", label)
+    return
 
   # Calculate the centered Y position
   var y_position = max((viewport_size.y - content_height) / 2, margin_top)
