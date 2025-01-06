a36a2b7fa48fc53415b3b6fe847583eb41f71a0c
m4ym4y <m4ym4y@users.noreply.github.com>
2025-01-06 00:09:45 -0800

    fill lobby with links by category

--- scenes/Lobby.gd
+++ scenes/Lobby.gd
@@ -7,14 +7,26 @@ extends Node3D
 @onready var _rng = RandomNumberGenerator.new()
 
 const FLOOR_WOOD = 0
+const FLOOR_CARPET = 11
+const FLOOR_MARBLE = 12
+
 const INTERNAL_HALL = 7
 const DIRECTIONS = [Vector3.FORWARD, Vector3.BACK, Vector3.LEFT, Vector3.RIGHT]
+const FLOORS = [FLOOR_WOOD, FLOOR_MARBLE, FLOOR_CARPET]
 
 func _get_hall_dir(pos):
 	var p = pos - Vector3.UP
-	for dir in DIRECTIONS:
+	var ori = _grid.get_cell_item_orientation(pos)
+	var dirs = []
+
+	if ori == 0 or ori == 10:
+		dirs = [Vector3.FORWARD, Vector3.BACK]
+	elif ori == 16 or ori == 22:
+		dirs = [Vector3.LEFT, Vector3.RIGHT]
+
+	for dir in dirs:
 		var cell = p + dir
-		if _grid.get_cell_item(cell) == FLOOR_WOOD:
+		if FLOORS.has(_grid.get_cell_item(cell)):
 			return -dir
 
 # Called when the node enters the scene tree for the first time.
