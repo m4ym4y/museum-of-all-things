7f2c3fe4fa0f18ce4fad0091288bdd372678c34b
m4ym4y <m4ym4y@users.noreply.github.com>
2025-01-22 23:26:59 -0800

    retab

--- scenes/util/RequestSync.gd
+++ scenes/util/RequestSync.gd
@@ -5,51 +5,51 @@ var protocol = "https://"
 var DELAY_MS = 10
 var USER_AGENT = "https://github.com/m4ym4y/wikipedia-museum"
 var COMMON_HEADERS = [
-	"accept: application/json; charset=utf-8",
-	"user-agent: " + USER_AGENT
+  "accept: application/json; charset=utf-8",
+  "user-agent: " + USER_AGENT
 ]
 
 # TODO: a version where we reuse the http client
 func request(url, headers=COMMON_HEADERS, verbose=true):
-	if OS.is_debug_build() and verbose:
-		print("fetching url ", url)
+  if OS.is_debug_build() and verbose:
+    print("fetching url ", url)
 
-	var http_client = HTTPClient.new()
-	var host_idx = url.find("/", len(protocol)) # first slash after protocol
-	var host = url.substr(0, host_idx)
-	var path = url.substr(host_idx)
+  var http_client = HTTPClient.new()
+  var host_idx = url.find("/", len(protocol)) # first slash after protocol
+  var host = url.substr(0, host_idx)
+  var path = url.substr(host_idx)
 
-	var err = http_client.connect_to_host(host, port, TLSOptions.client())
-	if err != OK:
-		return [err, 0, null]
+  var err = http_client.connect_to_host(host, port, TLSOptions.client())
+  if err != OK:
+    return [err, 0, null]
 
-	while (
-		http_client.get_status() == HTTPClient.STATUS_CONNECTING or
-		http_client.get_status() == HTTPClient.STATUS_RESOLVING
-	):
-		http_client.poll()
-		OS.delay_msec(DELAY_MS)
+  while (
+    http_client.get_status() == HTTPClient.STATUS_CONNECTING or
+    http_client.get_status() == HTTPClient.STATUS_RESOLVING
+  ):
+    http_client.poll()
+    OS.delay_msec(DELAY_MS)
 
-	if http_client.get_status() != HTTPClient.STATUS_CONNECTED:
-		return [FAILED, 0, null, null]
+  if http_client.get_status() != HTTPClient.STATUS_CONNECTED:
+    return [FAILED, 0, null, null]
 
-	http_client.request(HTTPClient.METHOD_GET, path, headers)
+  http_client.request(HTTPClient.METHOD_GET, path, headers)
 
-	while http_client.get_status() == HTTPClient.STATUS_REQUESTING:
-		http_client.poll()
-		OS.delay_msec(DELAY_MS)
+  while http_client.get_status() == HTTPClient.STATUS_REQUESTING:
+    http_client.poll()
+    OS.delay_msec(DELAY_MS)
 
-	if http_client.get_status() != HTTPClient.STATUS_BODY:
-		return [FAILED, 0, null, null]
+  if http_client.get_status() != HTTPClient.STATUS_BODY:
+    return [FAILED, 0, null, null]
 
-	var response = PackedByteArray()
-	while http_client.get_status() == HTTPClient.STATUS_BODY:
-		http_client.poll()
-		var chunk = http_client.read_response_body_chunk()
-		if chunk.size() > 0:
-			response.append_array(chunk)
-		OS.delay_msec(DELAY_MS)
+  var response = PackedByteArray()
+  while http_client.get_status() == HTTPClient.STATUS_BODY:
+    http_client.poll()
+    var chunk = http_client.read_response_body_chunk()
+    if chunk.size() > 0:
+      response.append_array(chunk)
+    OS.delay_msec(DELAY_MS)
 
-	var response_code = http_client.get_response_code()
-	var response_headers = http_client.get_response_headers()
-	return [OK, response_code, response_headers, response]
+  var response_code = http_client.get_response_code()
+  var response_headers = http_client.get_response_headers()
+  return [OK, response_code, response_headers, response]
--- scenes/util/ThreadSafeWorkQueue.gd
+++ scenes/util/ThreadSafeWorkQueue.gd
@@ -8,64 +8,64 @@ var _quitting = false
 var _queue_map = {}
 
 func _exit_tree():
-	set_quitting()
+  set_quitting()
 
 func set_quitting():
-	_quitting = true
+  _quitting = true
 
 func get_quitting():
-	return _quitting
+  return _quitting
 
 func set_current_exhibit(title):
-	_current_exhibit_lock.lock()
-	_current_exhibit = title
-	_current_exhibit_lock.unlock()
+  _current_exhibit_lock.lock()
+  _current_exhibit = title
+  _current_exhibit_lock.unlock()
 
 func get_current_exhibit():
-	_current_exhibit_lock.lock()
-	var res = _current_exhibit
-	_current_exhibit_lock.unlock()
-	return res
+  _current_exhibit_lock.lock()
+  var res = _current_exhibit
+  _current_exhibit_lock.unlock()
+  return res
 
 func _get_queue(name):
-	var res
-	_global_queue_lock.lock()
-	if not _queue_map.has(name):
-		_queue_map[name] = {
-			"exhibit_queues": {},
-			"lock": Mutex.new()
-		}
-	res = _queue_map[name]
-	_global_queue_lock.unlock()
-	return res
+  var res
+  _global_queue_lock.lock()
+  if not _queue_map.has(name):
+    _queue_map[name] = {
+      "exhibit_queues": {},
+      "lock": Mutex.new()
+    }
+  res = _queue_map[name]
+  _global_queue_lock.unlock()
+  return res
 
 func add_item(name, item, _exhibit=null, front=false):
-	var exhibit = _exhibit if _exhibit else get_current_exhibit()
-	var queue = _get_queue(name)
+  var exhibit = _exhibit if _exhibit else get_current_exhibit()
+  var queue = _get_queue(name)
 
-	queue.lock.lock()
-	if not queue.exhibit_queues.has(exhibit):
-		queue.exhibit_queues[exhibit] = []
+  queue.lock.lock()
+  if not queue.exhibit_queues.has(exhibit):
+    queue.exhibit_queues[exhibit] = []
 
-	if front:
-		queue.exhibit_queues[exhibit].push_front(item)
-	else:
-		queue.exhibit_queues[exhibit].append(item)
-	queue.lock.unlock()
+  if front:
+    queue.exhibit_queues[exhibit].push_front(item)
+  else:
+    queue.exhibit_queues[exhibit].append(item)
+  queue.lock.unlock()
 
 func process_queue(name):
-	var queue = _get_queue(name)
+  var queue = _get_queue(name)
 
-	while true:
-		if _quitting:
-			return null
+  while true:
+    if _quitting:
+      return null
 
-		var exhibit = get_current_exhibit()
-		queue.lock.lock()
-		var item
-		if queue.exhibit_queues.has(exhibit):
-			item = queue.exhibit_queues[exhibit].pop_front()
-		queue.lock.unlock()
-		if item:
-			return item
-		OS.delay_msec(QUEUE_WAIT_TIMEOUT_MS)
+    var exhibit = get_current_exhibit()
+    queue.lock.lock()
+    var item
+    if queue.exhibit_queues.has(exhibit):
+      item = queue.exhibit_queues[exhibit].pop_front()
+    queue.lock.unlock()
+    if item:
+      return item
+    OS.delay_msec(QUEUE_WAIT_TIMEOUT_MS)
