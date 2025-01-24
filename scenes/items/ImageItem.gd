494f2da528c2f19998cb90c60efac0c9ca7bb929
m4ym4y <m4ym4y@users.noreply.github.com>
2025-01-23 22:07:42 -0800

    fix: weird ghost images bc image texture was not local to scene

--- scenes/items/ImageItem.gd
+++ scenes/items/ImageItem.gd
@@ -47,8 +47,9 @@ func _on_image_loaded(url, image, _ctx):
 
   DataManager.loaded_image.disconnect(_on_image_loaded)
   _image = image
-  if _image.get_size().length() > 0:
-    texture.set_size_override(_image.get_size())
+  var size = _image.get_size()
+  if size.length() > 0:
+    texture.set_size_override(size)
     material_override.set_shader_parameter("texture_albedo", _image)
 
   var label = $Label
