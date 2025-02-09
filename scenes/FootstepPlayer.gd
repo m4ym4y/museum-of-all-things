bc5fcddcd0485a5f5a202373163fe0d8562d7c07
m4ym4y <m4ym4y@users.noreply.github.com>
2025-02-08 21:48:28 -0800

    move footstep sfx to footstep player

---
+++ scenes/FootstepPlayer.gd
@@ -0,0 +1,40 @@
+extends Node3D
+
+@onready var _footsteps = {
+  "hard": $FootstepsHard,
+  "soft": $FootstepsSoft
+}
+
+var _default_floor_type = "hard"
+var _floor_material_map = {
+  11: "soft", # carpet
+}
+
+var _grid
+func init(grid):
+  _grid = grid
+
+func play_footsteps(on_floor, velocity, max_speed):
+  if not _grid:
+    return
+
+  var flat_velocity = Vector2(velocity.x, velocity.z).length()
+
+  if on_floor and flat_velocity > 0:
+    var floor_cell = Util.worldToGrid(position) - Vector3.UP
+    var floor_cell_type = _grid.get_cell_item(floor_cell)
+    var step_type = _floor_material_map.get(
+      floor_cell_type,
+      _default_floor_type
+    )
+
+    for step in _footsteps.keys():
+      if step == step_type:
+        _footsteps[step].pitch_scale = flat_velocity / max_speed
+        if not _footsteps[step].playing:
+          _footsteps[step].play()
+      else:
+        _footsteps[step].stop()
+  else:
+    for step in _footsteps.keys():
+      _footsteps[step].stop()
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -17,15 +17,6 @@ var _joy_right_x = JOY_AXIS_RIGHT_X
 var _joy_right_y = JOY_AXIS_RIGHT_Y
 
 @onready var camera = get_node("Pivot/Camera3D")
-@onready var _footsteps = {
-  "hard": $FootstepsHard,
-  "soft": $FootstepsSoft
-}
-
-var _default_floor_type = "hard"
-var _floor_material_map = {
-  11: "soft", # carpet
-}
 
 @export var smooth_movement = false
 @export var dampening = 0.01
@@ -43,9 +34,8 @@ func _ready():
   crouch_speed = (starting_height - crouching_height) / crouch_time
 
 # set grid so we can read floor type on it
-var _grid
 func set_grid(grid):
-  _grid = grid
+  $FootstepPlayer.init(grid)
 
 func pause():
   _enabled = false
@@ -117,7 +107,7 @@ func _physics_process(delta):
     $Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
     camera_v *= 0.95
 
-  _play_footsteps()
+  $FootstepPlayer.play_footsteps(is_on_floor(), velocity, max_speed)
 
   if Input.is_action_pressed("jump") and is_on_floor():
     velocity.y = jump_impulse
@@ -133,27 +123,3 @@ func _physics_process(delta):
     if collider and collider.has_method("interact"):
       collider.interact()
 
-func _play_footsteps():
-  if not _grid:
-    return
-
-  var flat_velocity = Vector2(velocity.x, velocity.z).length()
-
-  if is_on_floor() and flat_velocity > 0:
-    var floor_cell = Util.worldToGrid(position) - Vector3.UP
-    var floor_cell_type = _grid.get_cell_item(floor_cell)
-    var step_type = _floor_material_map.get(
-      floor_cell_type,
-      _default_floor_type
-    )
-
-    for step in _footsteps.keys():
-      if step == step_type:
-        _footsteps[step].pitch_scale = flat_velocity / max_speed
-        if not _footsteps[step].playing:
-          _footsteps[step].play()
-      else:
-        _footsteps[step].stop()
-  else:
-    for step in _footsteps.keys():
-      _footsteps[step].stop()
