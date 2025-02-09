c5f5923ebfe5c4f0bd8130ad2bf36ed34ad89a0b
m4ym4y <m4ym4y@users.noreply.github.com>
2025-02-09 14:24:09 -0800

    fade into main menu

--- scenes/menu/MainMenu.gd
+++ scenes/menu/MainMenu.gd
@@ -3,13 +3,26 @@ extends Control
 signal start
 signal settings
 
+var fade_in_start = Color(0.0, 0.0, 0.0, 1.0)
+var fade_in_end = Color(0.0, 0.0, 0.0, 0.0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   _on_visibility_changed()
+  call_deferred("_start_fade_in")
 
 func _on_visibility_changed():
   if visible:
-    $MarginContainer/VBoxContainer/HBoxContainer/Start.grab_focus()
+    $MarginContainer/VBoxContainer/Start.grab_focus()
+
+func _start_fade_in():
+  $FadeIn.color = fade_in_start
+  $FadeInStage2.color = fade_in_start
+  var tween = get_tree().create_tween()
+  tween.tween_property($FadeIn, "color", fade_in_end, 1.5)
+  tween.tween_property($FadeInStage2, "color", fade_in_end, 1.5)
+  tween.set_trans(Tween.TRANS_LINEAR)
+  tween.set_ease(Tween.EASE_IN_OUT)
 
 func _on_start_pressed():
   emit_signal("start")
