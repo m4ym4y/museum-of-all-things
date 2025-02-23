d59af3c442f93d549d799ee92c76ac2ad39b5fe8
m4ym4y <m4ym4y@users.noreply.github.com>
2025-02-23 01:19:25 -0800

    integrate new ambience events

--- scenes/AmbienceController.gd
+++ scenes/AmbienceController.gd
@@ -9,6 +9,58 @@ var _ambience_tracks = [
   preload("res://assets/sound/Global Ambience/Global ambience 4.ogg"),
 ]
 
+var _ambient_voice_space_min = 60
+var _ambient_voice_space_max = 480
+var _ambient_voices = [
+  preload("res://assets/sound/Voices/Voices 1.ogg"),
+  preload("res://assets/sound/Voices/Voices 2.ogg"),
+  preload("res://assets/sound/Voices/Voices 3.ogg"),
+  preload("res://assets/sound/Voices/Voices 4.ogg"),
+  preload("res://assets/sound/Voices/Voices 5.ogg"),
+  preload("res://assets/sound/Voices/Voices 6.ogg"),
+  preload("res://assets/sound/Voices/Voices 7.ogg"),
+  preload("res://assets/sound/Voices/Voices 8.ogg"),
+  preload("res://assets/sound/Voices/Voices 9.ogg"),
+]
+
+var _ambience_event_space_min = 30
+var _ambience_event_space_max = 180
+var _ambience_events_weighted = [
+  [1, preload("res://assets/sound/Easter Eggs/Bird Cry 1.ogg")],
+  [1, preload("res://assets/sound/Easter Eggs/Bird Flapping 1.ogg")],
+  [1, preload("res://assets/sound/Easter Eggs/Peepers 1.ogg")],
+  [5, preload("res://assets/sound/Easter Eggs/Cricket Loop.ogg")],
+  [5, preload("res://assets/sound/Easter Eggs/Easter Eggs pen drop 1.ogg")],
+  [15, preload("res://assets/sound/Easter Eggs/Random Ambience 1.ogg")],
+  [15, preload("res://assets/sound/Easter Eggs/Random Ambience 2.ogg")],
+  [15, preload("res://assets/sound/Easter Eggs/Random Ambience 3.ogg")],
+  [15, preload("res://assets/sound/Easter Eggs/Random Ambience 4.ogg")],
+]
+
+func _ambience_voice_timer():
+  get_tree().create_timer(randi_range(_ambient_voice_space_min, _ambient_voice_space_max)).timeout.connect(_play_ambience_voice)
+
+func _ambience_event_timer():
+  get_tree().create_timer(randi_range(_ambience_event_space_min, _ambience_event_space_max)).timeout.connect(_play_ambience_event)
+
+func _play_ambience_voice():
+  var player = _create_player(_ambient_voices[randi() % len(_ambient_voices)])
+  player.finished.connect(_clean_player.bind(player))
+  _ambience_voice_timer()
+
+func _play_ambience_event():
+  var weight_sum = 0
+  for ev in _ambience_events_weighted:
+    weight_sum += ev[0]
+  var choice = randi_range(1, weight_sum)
+  for ev in _ambience_events_weighted:
+    choice -= ev[0]
+    if choice <= 0:
+      var player = _create_player(ev[1])
+      player.finished.connect(_clean_player.bind(player))
+      break
+  _ambience_event_timer
+
 func _random_track():
   return _ambience_tracks[randi() % len(_ambience_tracks)]
 
@@ -27,10 +79,10 @@ func _fade_between(audio1, res2, duration):
   var tween = get_tree().create_tween()
   tween.tween_property(audio2, "volume_db", 0, duration)
   tween.tween_property(audio1, "volume_db", -80, duration)
-  tween.finished.connect(_fade_finished.bind(audio1))
+  tween.finished.connect(_clean_player.bind(audio1))
   return audio2
 
-func _fade_finished(audio1):
+func _clean_player(audio1):
   audio1.queue_free()
 
 func _next_track():
@@ -44,3 +96,5 @@ func _start_playing():
   _current_player.volume_db = 0.0
   $Timer.timeout.connect(_next_track)
   $Timer.start()
+  _ambience_event_timer()
+  _ambience_voice_timer()
