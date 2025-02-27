208a4f2ceffeb61f16950ee77cfaa9a520dc6f23
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2025-02-27 01:30:38 -0300

    Add support for languages starting with portuguese

--- scenes/menu/MainMenu.gd
+++ scenes/menu/MainMenu.gd
@@ -6,6 +6,7 @@ signal settings
 var fade_in_start = Color(0.0, 0.0, 0.0, 1.0)
 var fade_in_end = Color(0.0, 0.0, 0.0, 0.0)
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   _on_visibility_changed()
@@ -36,3 +37,25 @@ func _on_quit_button_pressed():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
   pass
+
+func _on_language_item_selected(index: int) -> void:
+  if index == 0:
+    TranslationServer.set_locale("en")
+  if index == 1:
+    TranslationServer.set_locale("pt")
+  # if index == 2:
+  #   TranslationServer.set_locale("fr")
+
+  var lang = TranslationServer.get_locale()
+
+  ExhibitFetcher.WIKIPEDIA_PREFIX = "https://" + lang + ".wikipedia.org/wiki/"
+
+  ExhibitFetcher.search_endpoint = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=json&list=search&srprop=title&srsearch="
+  ExhibitFetcher.random_endpoint = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=json&generator=random&grnnamespace=0&prop=info"
+
+  ExhibitFetcher.wikitext_endpoint = "https://" + lang + ".wikipedia.org/w/api.php?action=query&prop=revisions|extracts|pageprops&ppprop=wikibase_item&explaintext=true&rvprop=content&format=json&redirects=1&titles="
+  ExhibitFetcher.images_endpoint = "https://" + lang + ".wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata|url&iiurlwidth=640&iiextmetadatafilter=LicenseShortName|Artist&format=json&redirects=1&titles="
+  ExhibitFetcher.wikidata_endpoint = "https://www.wikidata.org/w/api.php?action=wbgetclaims&uselang=" + lang + "&format=json&entity="
+
+  ExhibitFetcher.wikimedia_commons_category_images_endpoint = "https://commons.wikimedia.org/w/api.php?action=query&uselang=" + lang + "&generator=categorymembers&gcmtype=file&gcmlimit=max&prop=imageinfo&iiprop=url|extmetadata&iiurlwidth=640&iiextmetadatafilter=Artist|LicenseShortName&format=json&gcmtitle="
+  ExhibitFetcher.wikimedia_commons_gallery_images_endpoint = "https://commons.wikimedia.org/w/api.php?action=query&uselang=" + lang + "&generator=images&gimlimit=max&prop=imageinfo&iiprop=url|extmetadata&iiurlwidth=640&iiextmetadatafilter=Artist|LicenseShortName&format=json&titles="
