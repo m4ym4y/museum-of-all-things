8e0c902e711796b980f64c8e71b99f977d3da180
David Snopek <dsnopek@gmail.com>
2025-02-28 22:09:35 -0600

    Add web export

--- scenes/util/DataManager.gd
+++ scenes/util/DataManager.gd
@@ -7,22 +7,28 @@ signal loaded_image(url: String, image: Image)
 @onready var _xr = Util.is_xr()
 
 var TEXTURE_QUEUE = "Textures"
+var TEXTURE_FRAME_PACING = 6
 var _fs_lock = Mutex.new()
 var _texture_load_thread_pool_size = 5
 var _texture_load_thread_pool = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  var cache_dir = CacheControl.cache_dir
-  var dir = DirAccess.open(cache_dir)
-  if not dir:
-    DirAccess.make_dir_recursive_absolute(cache_dir)
-    if OS.is_debug_build():
-      print("cache directory created at '%s'" + cache_dir)
-  for _i in range(_texture_load_thread_pool_size):
-    var thread = Thread.new()
-    thread.start(_texture_load_thread_loop)
-    _texture_load_thread_pool.append(thread)
+  WorkQueue.setup_queue(TEXTURE_QUEUE, TEXTURE_FRAME_PACING)
+
+  if not Util.is_web():
+    var cache_dir = CacheControl.cache_dir
+    var dir = DirAccess.open(cache_dir)
+    if not dir:
+      DirAccess.make_dir_recursive_absolute(cache_dir)
+      if OS.is_debug_build():
+        print("cache directory created at '%s'" + cache_dir)
+
+  if Util.is_using_threads():
+    for _i in range(_texture_load_thread_pool_size):
+      var thread = Thread.new()
+      thread.start(_texture_load_thread_loop)
+      _texture_load_thread_pool.append(thread)
 
 func _exit_tree():
   WorkQueue.set_quitting()
@@ -31,22 +37,69 @@ func _exit_tree():
 
 func _texture_load_thread_loop():
   while not WorkQueue.get_quitting():
-    var item = WorkQueue.process_queue(TEXTURE_QUEUE)
-    if not item:
-      continue
+    _texture_load_item()
 
-    var data = _read_url(item.url)
+func _process(delta: float) -> void:
+  if not Util.is_using_threads():
+    _texture_load_item()
 
-    if data:
-      _create_and_emit_image(item.url, data, item.ctx)
-    else:
-      var result = RequestSync.request(item.url, COMMON_HEADERS)
-      if result[0] != OK:
-        push_error("failed to fetch image ", result[1], " ", item.url)
+func _texture_load_item():
+  var item = WorkQueue.process_queue(TEXTURE_QUEUE)
+  if not item:
+    return
+
+  match item.type:
+    "request":
+        var data = _read_url(item.url)
+
+        if data:
+          _load_image(item.url, data, item.ctx)
+        else:
+          var request_url = item.url
+          request_url += ('&' if '?' in request_url else '?') + "origin=*"
+
+          var handle_result = func(result):
+            if result[0] != OK:
+              push_error("failed to fetch image ", result[1], " ", item.url)
+            else:
+              data = result[3]
+              _write_url(item.url, data)
+              _load_image(item.url, data, item.ctx)
+
+          if Util.is_web():
+            RequestSync.request_async(request_url, COMMON_HEADERS).completed.connect(handle_result)
+          else:
+            handle_result.call(RequestSync.request(request_url, COMMON_HEADERS))
+
+    "load":
+      var data = item.data
+
+      var fmt = _detect_image_type(data)
+      var image = Image.new()
+      if fmt == "PNG":
+        image.load_png_from_buffer(data)
+      elif fmt == "JPEG":
+        image.load_jpg_from_buffer(data)
+      elif fmt == "SVG":
+        image.load_svg_from_buffer(data)
+      elif fmt == "WebP":
+        image.load_webp_from_buffer(data)
       else:
-        data = result[3]
-        _write_url(item.url, data)
-        _create_and_emit_image(item.url, data, item.ctx)
+        return
+
+      if image.get_width() == 0:
+        return
+
+      _generate_mipmaps(item.url, image, item.ctx)
+
+    "generate_mipmaps":
+      var image = item.image
+      image.generate_mipmaps()
+      _create_and_emit_texture(item.url, image, item.ctx)
+
+    "create_and_emit_texture":
+      var texture = ImageTexture.create_from_image(item.image)
+      _emit_image(item.url, texture, item.ctx)
 
 func _get_hash(input: String) -> String:
   var context = HashingContext.new()
@@ -88,6 +141,8 @@ func _detect_image_type(data: PackedByteArray) -> String:
   return "Unknown"
 
 func _write_url(url: String, data: PackedByteArray) -> void:
+  if Util.is_web():
+    return
   _fs_lock.lock()
   var filename = _get_hash(url)
   var f = FileAccess.open(CacheControl.cache_dir + filename, FileAccess.WRITE)
@@ -99,6 +154,8 @@ func _write_url(url: String, data: PackedByteArray) -> void:
   _fs_lock.unlock()
 
 func _read_url(url: String):
+  if Util.is_web():
+    return null
   _fs_lock.lock()
   var filename = _get_hash(url)
   var file_path = CacheControl.cache_dir + filename
@@ -113,6 +170,8 @@ func _read_url(url: String):
     return null
 
 func _url_exists(url: String):
+  if Util.is_web():
+    return false
   _fs_lock.lock()
   var filename = _get_hash(url)
   var res = FileAccess.file_exists(CacheControl.cache_dir + filename)
@@ -128,30 +187,10 @@ func load_json_data(url: String):
     return null
 
 func save_json_data(url: String, json: Dictionary):
+  if Util.is_web():
+    return
   var data = JSON.stringify(json).to_utf8_buffer()
   _write_url(url, data)
-
-func _create_and_emit_image(url, data, ctx):
-  var fmt = _detect_image_type(data)
-  var image = Image.new()
-  if fmt == "PNG":
-    image.load_png_from_buffer(data)
-  elif fmt == "JPEG":
-    image.load_jpg_from_buffer(data)
-  elif fmt == "SVG":
-    image.load_svg_from_buffer(data)
-  elif fmt == "WebP":
-    image.load_webp_from_buffer(data)
-  else:
-    return null
-
-  if image.get_width() == 0:
-    return null
-
-  image.generate_mipmaps()
-
-  var texture = ImageTexture.create_from_image(image)
-  _emit_image(url, texture, ctx)
 
 func _emit_image(url, texture, ctx):
   if texture == null:
@@ -160,6 +199,31 @@ func _emit_image(url, texture, ctx):
 
 func request_image(url, ctx=null):
   WorkQueue.add_item(TEXTURE_QUEUE, {
+    "type": "request",
     "url": url,
     "ctx": ctx
   })
+
+func _load_image(url, data, ctx=null):
+  WorkQueue.add_item(TEXTURE_QUEUE, {
+    "type": "load",
+    "url": url,
+    "data": data,
+    "ctx": ctx,
+  }, null, true)
+
+func _generate_mipmaps(url, image, ctx=null):
+  WorkQueue.add_item(TEXTURE_QUEUE, {
+    "type": "generate_mipmaps",
+    "url": url,
+    "image": image,
+    "ctx": ctx,
+  }, null, true)
+
+func _create_and_emit_texture(url, image, ctx=null):
+  WorkQueue.add_item(TEXTURE_QUEUE, {
+    "type": "create_and_emit_texture",
+    "url": url,
+    "image": image,
+    "ctx": ctx,
+  }, null, true)
