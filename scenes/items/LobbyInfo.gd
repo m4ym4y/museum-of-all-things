82a3d4d68c9377ae307858768038eb2e0854072d
stoofin <44447892+stoofin@users.noreply.github.com>
2025-03-28 01:17:54 -0700

    mipmaps for LobbyInfo and RichTextItem
    read back asynchronously and generated on a separate thread

--- scenes/items/LobbyInfo.gd
+++ scenes/items/LobbyInfo.gd
@@ -4,4 +4,9 @@ extends MeshInstance3D
 @export_multiline var text = "[Replace me]"
 
 func _ready() -> void:
-	$SubViewport/Control/RichTextLabel.text = text
+  $SubViewport/Control/RichTextLabel.text = text
+  if not Engine.is_editor_hint():
+    MipmapThread.get_viewport_texture_with_mipmaps($SubViewport, func(texture):
+      $Sprite3D.texture = texture
+      $SubViewport.queue_free()
+    )
---
+++ scenes/util/MipmapThread.gd
@@ -0,0 +1,61 @@
+extends Node
+
+var thread: Thread
+
+var mutex := Mutex.new()
+var semaphore := Semaphore.new()
+
+var should_exit := false
+var queue: Array[Callable] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  thread = Thread.new()
+  thread.start(_thread_loop)
+
+func _exit_tree() -> void:
+  mutex.lock()
+  should_exit = true
+  mutex.unlock()
+  semaphore.post()
+
+  thread.wait_to_finish()
+
+func _thread_loop():
+  while true:
+    semaphore.wait()
+    mutex.lock()
+    if should_exit:
+        mutex.unlock()
+        return
+    var task: Callable = queue.pop_front()
+    mutex.unlock()
+    task.call()
+
+func queue_work(c: Callable):
+  mutex.lock()
+  queue.push_back(c)
+  mutex.unlock()
+  semaphore.post()
+
+func get_mipmapped_texture_async(texture: Texture2D, callback: Callable):
+  var width = texture.get_width()
+  var height = texture.get_height()
+  var rid = texture.get_rid()
+  var format = RenderingServer.texture_get_format(rid)
+  var rd_rid = RenderingServer.texture_get_rd_texture(rid)
+  #print("Requesting download of ", rd_rid)
+  RenderingServer.get_rendering_device().texture_get_data_async(rd_rid, 0, func(array) -> void:
+    #print("Download complete: ", array.size())
+    queue_work(func() -> void:
+      var img = Image.create_from_data(width, height, false, format, array)
+      img.generate_mipmaps()
+      var mipmapped_texture := ImageTexture.create_from_image(img)
+      if callback.is_valid():
+        callback.call_deferred(mipmapped_texture)
+    )
+  )
+
+func get_viewport_texture_with_mipmaps(subviewport: SubViewport, callback: Callable):
+  await RenderingServer.frame_post_draw
+  get_mipmapped_texture_async(subviewport.get_texture(), callback)
